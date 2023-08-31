bfcc380e06fec34b84757c4c4cbd7995f488551c
JesusRuiz02 <79652924+JesusRuiz02@users.noreply.github.com>
2023-08-31 14:47:07 -0600

    aaaaaaa

--- Assets/Scripts/DestroyingObjects.cs
+++ Assets/Scripts/DestroyingObjects.cs
@@ -1,8 +1,13 @@
-
+using System;
 using UnityEngine;
 
 public class DestroyingObjects : MonoBehaviour
 {
+  private void Start()
+  {
+    Destroy(gameObject,16f);
+  }
+
   void OnTriggerEnter(Collider col)
   {
     if (col.CompareTag("DestroyObject"))
